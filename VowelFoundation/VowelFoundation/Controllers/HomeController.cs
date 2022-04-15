495e94d16537197026d227ba3da73575ad14b70c
sanjeevvowel <sanjeev@voweldigitalworld.com>
2022-04-15 17:04:57 +0530

    changes

---
+++ VowelFoundation/VowelFoundation/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace VowelFoundation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult Causes()
+        {
+            return View();
+        }
+        public ActionResult Event()
+        {
+            return View();
+        }
+        public ActionResult Blog()
+        {
+            return View();
+        }
+        public ActionResult BlogDetail()
+        {
+            return View();
+        }
+        public ActionResult ContactUs()
+        {
+            return View();
+        }
+        public ActionResult DonateNow()
+        {
+            return View();
+        }
+    }
+}
